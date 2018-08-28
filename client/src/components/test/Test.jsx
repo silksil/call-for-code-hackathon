cebf0406fdbc593da4de362fbf8a8f55520b8e88
StratosTso <stratos.tso@gmail.com>
2018-08-28 13:35:10 +0200

    Updated Map with embedded User data

--- client/src/components/test/Test.jsx
+++ client/src/components/test/Test.jsx
@@ -4,16 +4,16 @@ import requireAuth from '../requireAuth';
 import ReactDOM from 'react-dom'
 import mapboxgl from 'mapbox-gl'
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RyYXRvc3RzbyIsImEiOiJjamttZGUyMjMyYXd0M3BwOWhidDZ5am05In0.u6Lz6obeHCPxr8tKb9Km-g'; //Tso
 
 class Test extends Component {
 
   constructor(props: Props) {
     super(props);
     this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 1.5
+      lng: 32.73,
+      lat: 39.97,
+      zoom: 12
     };
   }
 
@@ -22,20 +22,153 @@ class Test extends Component {
 
     const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
+      style: 'mapbox://styles/stratostso/cjldjmln47dg72socw3b4jvdw',
       center: [lng, lat],
       zoom
     });
 
-    map.on('move', () => {
-      const { lng, lat } = map.getCenter();
-
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+    map.on('load', () => {
+      map.addLayer({
+          "id": "points",
+          "type": "symbol",
+          "source": {
+              "type": "geojson",
+              "data": {
+                  "type": "FeatureCollection",
+                  "features": [
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.74, 39.97]
+                      },
+                      "properties": {
+                          "name": "Annie Wilkis",
+                          "title": "nurse",
+                          "icon": "nurse",
+                          "organisation": "Red Cross",
+                          "skills": "High Skilled"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.72,39.96]
+                      },
+                      "properties": {
+                          "name": "Mallory Knox",
+                          "title": "nurse",
+                          "icon": "nurse",
+                          "organisation": "Red Cross",
+                          "skills": "Low Skilled"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.745,39.94]
+                      },
+                      "properties": {
+                          "name": "Samara Morgan",
+                          "title": "nurse",
+                          "icon": "nurse",
+                          "organisation": "Doctors Without Borders",
+                          "skills": "Trainee"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.75, 39.97]
+                      },
+                      "properties": {
+                          "name":"Dexter Morgan",
+                          "title": "doctor",
+                          "icon": "doctor",
+                          "organisation": "Red Cross",
+                          "skills": "Blood Specialist"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.76, 39.96]
+                      },
+                      "properties": {
+                          "name":"Norman Bates",
+                          "title": "doctor",
+                          "icon": "doctor",
+                          "organisation": "Doctors Without Borders",
+                          "skills": "Anesthisiologist"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.764, 39.964]
+                      },
+                      "properties": {
+                          "name":"Swenney Todd",
+                          "title": "doctor",
+                          "icon": "doctor",
+                          "organisation": "Doctors Without Borders",
+                          "skills": "Surgent"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.738, 39.979]
+                      },
+                      "properties": {
+                          "name":"Hannibal Lecter",
+                          "title": "engineer",
+                          "icon": "engineer",
+                          "organisation": "Engineers organisation",
+                          "skills": "CEO"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.733, 39.93]
+                      },
+                      "properties": {
+                          "name":"Michael Mayers",
+                          "title": "engineer",
+                          "icon": "engineer",
+                          "organisation": "Engineers organisation",
+                          "skills": "Structural"
+                      }
+                    },
+                    {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": [32.746, 39.936]
+                      },
+                      "properties": {
+                          "name":"Stu Macher",
+                          "title": "engineer",
+                          "icon": "engineer",
+                          "organisation": "Engineers organisation",
+                          "skills": "Statical Engineer"
+                      }
+                    },
+                ]
+              }
+          },
+          "layout": {"icon-image": "{icon}"}
       });
     });
+
   }
 
   render() {
