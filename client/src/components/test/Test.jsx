67cb2037cca7e937060d7c68f099192f9cd678f0
StratosTso <stratos.tso@gmail.com>
2018-08-29 16:29:35 +0200

    checkbox is added / bar map changed

--- client/src/components/test/Test.jsx
+++ client/src/components/test/Test.jsx
@@ -6,9 +6,10 @@ import mapboxgl from 'mapbox-gl'
 
 import '../../style/map-styles.css';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3RyYXRvc3RzbyIsImEiOiJjamttZGUyMjMyYXd0M3BwOWhidDZ5am05In0.u6Lz6obeHCPxr8tKb9Km-g'; //Tso
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RyYXRvc3RzbyIsImEiOiJjamttZGUyMjMyYXd0M3BwOWhidDZ5am05In0.u6Lz6obeHCPxr8tKb9Km-g';
 
 class Test extends Component {
+
 
   constructor(props: Props) {
     super(props);
@@ -22,6 +23,8 @@ class Test extends Component {
   componentDidMount() {
     const { lng, lat, zoom } = this.state;
 
+    const filterGroup = document.getElementById('filter-group');
+
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/stratostso/cjldjl0pa63to2sp6yl9cdk2j',
@@ -29,150 +32,184 @@ class Test extends Component {
       zoom
     });
 
+    var gpsLocation = {
+            "type": "FeatureCollection",
+            "features": [
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.74, 39.97]
+                },
+                "properties": {
+                    "username": "Annie Wilkis",
+                    "title": "Nurse",
+                    "icon": "nurse",
+                    "organisation": "Red Cross",
+                    "skills": "High Skilled"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.72,39.96]
+                },
+                "properties": {
+                    "username": "Mallory Knox",
+                    "title": "Nurse",
+                    "icon": "nurse",
+                    "organisation": "Red Cross",
+                    "skills": "Low Skilled"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.745,39.955]
+                },
+                "properties": {
+                    "username": "Samara Morgan",
+                    "title": "Nurse",
+                    "icon": "nurse",
+                    "organisation": "Doctors Without Borders",
+                    "skills": "Trainee"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.75, 39.97]
+                },
+                "properties": {
+                    "username":"Dexter Morgan",
+                    "title": "Doctor",
+                    "icon": "doctor",
+                    "organisation": "Red Cross",
+                    "skills": "Blood Specialist"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.76, 39.96]
+                },
+                "properties": {
+                    "username":"Norman Bates",
+                    "title": "Doctor",
+                    "icon": "doctor",
+                    "organisation": "Doctors Without Borders",
+                    "skills": "Anesthisiologist"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.764, 39.964]
+                },
+                "properties": {
+                    "username":"Swenney Todd",
+                    "title": "Doctor",
+                    "icon": "doctor",
+                    "organisation": "Doctors Without Borders",
+                    "skills": "Surgent"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.738, 39.979]
+                },
+                "properties": {
+                    "username":"Hannibal Lecter",
+                    "title": "Engineer",
+                    "icon": "engineer",
+                    "organisation": "Engineers organisation",
+                    "skills": "CEO"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.733, 39.976]
+                },
+                "properties": {
+                    "username":"Patrick Bateman",
+                    "title": "Engineer",
+                    "icon": "engineer",
+                    "organisation": "Engineers organisation",
+                    "skills": "Structural"
+                }
+              },
+              {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [32.746, 39.966]
+                },
+                "properties": {
+                    "username":"Stu Macher",
+                    "title": "Engineer",
+                    "icon": "engineer",
+                    "organisation": "Engineers organisation",
+                    "skills": "Statical Engineer"
+                }
+              },
+            ]
+    };
+
     map.on('load', () => {
-      map.addLayer({
-          "id": "users",
-          "type": "symbol",
-          "source": {
-              "type": "geojson",
-              "data": {
-                  "type": "FeatureCollection",
-                  "features": [
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.74, 39.97]
-                      },
-                      "properties": {
-                          "username": "Annie Wilkis",
-                          "title": "Nurse",
-                          "icon": "nurse",
-                          "organisation": "Red Cross",
-                          "skills": "High Skilled"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.72,39.96]
-                      },
-                      "properties": {
-                          "username": "Mallory Knox",
-                          "title": "Nurse",
-                          "icon": "nurse",
-                          "organisation": "Red Cross",
-                          "skills": "Low Skilled"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.745,39.955]
-                      },
-                      "properties": {
-                          "username": "Samara Morgan",
-                          "title": "Nurse",
-                          "icon": "nurse",
-                          "organisation": "Doctors Without Borders",
-                          "skills": "Trainee"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.75, 39.97]
-                      },
-                      "properties": {
-                          "username":"Dexter Morgan",
-                          "title": "Doctor",
-                          "icon": "doctor",
-                          "organisation": "Red Cross",
-                          "skills": "Blood Specialist"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.76, 39.96]
-                      },
-                      "properties": {
-                          "username":"Norman Bates",
-                          "title": "Doctor",
-                          "icon": "doctor",
-                          "organisation": "Doctors Without Borders",
-                          "skills": "Anesthisiologist"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.764, 39.964]
-                      },
-                      "properties": {
-                          "username":"Swenney Todd",
-                          "title": "Doctor",
-                          "icon": "doctor",
-                          "organisation": "Doctors Without Borders",
-                          "skills": "Surgent"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.738, 39.979]
-                      },
-                      "properties": {
-                          "username":"Hannibal Lecter",
-                          "title": "Engineer",
-                          "icon": "engineer",
-                          "organisation": "Engineers organisation",
-                          "skills": "CEO"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.733, 39.976]
-                      },
-                      "properties": {
-                          "username":"Patrick Bateman",
-                          "title": "Engineer",
-                          "icon": "engineer",
-                          "organisation": "Engineers organisation",
-                          "skills": "Structural"
-                      }
-                    },
-                    {
-                      "type": "Feature",
-                      "geometry": {
-                          "type": "Point",
-                          "coordinates": [32.746, 39.966]
-                      },
-                      "properties": {
-                          "username":"Stu Macher",
-                          "title": "Engineer",
-                          "icon": "engineer",
-                          "organisation": "Engineers organisation",
-                          "skills": "Statical Engineer"
-                      }
-                    },
-                ]
-              }
-          },
-          "layout": {"icon-image": "{icon}"}
+      map.addSource("gpsLocation", {
+          "type": "geojson",
+          "data": gpsLocation
       });
 
+      gpsLocation.features.forEach(function (feature) {
+          var symbol = feature.properties['icon'];
+          var layerID = 'poi-' + symbol;
+
+          // Add a layer for this symbol type if it hasn't been added already.
+          if (!map.getLayer(layerID)) {
+              map.addLayer({
+                  "id": layerID,
+                  "type": "symbol",
+                  "source": "gpsLocation",
+                  "layout": {
+                      "icon-image": symbol,
+                      "icon-allow-overlap": true
+                  },
+                  "filter": ["==", "icon", symbol]
+              });
+
+              // Add checkbox and label elements for the layer.
+              var input = document.createElement('input');
+              input.type = 'checkbox';
+              input.id = layerID;
+              input.checked = true;
+              filterGroup.appendChild(input);
+
+              var label = document.createElement('label');
+              label.setAttribute('for', layerID);
+              label.textContent = symbol;
+              filterGroup.appendChild(label);
+
+              // When the checkbox changes, update the visibility of the layer.
+              input.addEventListener('change', function (e) {
+                  map.setLayoutProperty(layerID, 'visibility',
+                      e.target.checked ? 'visible' : 'none');
+              });
+          }
+      });
+      ///////////////////Nurse PopUp
       map.on('click', function(e) {
         var features = map.queryRenderedFeatures(e.point, {
-          layers: ['users'] // replace this with the name of the layer
+          layers: ['poi-nurse']
         });
 
         if (!features.length) {
@@ -189,17 +226,69 @@ class Test extends Component {
           .addTo(map);
       });
 
-      // Change the cursor to a pointer when the mouse is over the places layer.
-      map.on('mouseenter', 'users', function () {
+      map.on('mouseenter', 'poi-nurse', function () {
           map.getCanvas().style.cursor = 'pointer';
       });
 
-      // Change it back to a pointer when it leaves.
-      map.on('mouseleave', 'users', function () {
+      map.on('mouseleave', 'poi-nurse', function () {
           map.getCanvas().style.cursor = '';
       });
 
+      ///////////////////Doctor PopUp
+      map.on('click', function(e) {
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ['poi-doctor']
+        });
 
+        if (!features.length) {
+          return;
+        }
+
+        var feature = features[0];
+
+        var popup = new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(feature.geometry.coordinates)
+          .setHTML(
+            '<h3>'+feature.properties.username+'</h3><br /><p><h2><b>' + feature.properties.title + '</b></h2><br />Skills: ' + feature.properties.skills + '<br /><p>Organisation: ' + feature.properties.organisation  + '<br /></p>')
+          .setLngLat(feature.geometry.coordinates)
+          .addTo(map);
+      });
+
+      map.on('mouseenter', 'poi-doctor', function () {
+          map.getCanvas().style.cursor = 'pointer';
+      });
+
+      map.on('mouseleave', 'poi-doctor', function () {
+          map.getCanvas().style.cursor = '';
+      });
+
+      ///////////////////Engineer PopUp
+      map.on('click', function(e) {
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ['poi-engineer']
+        });
+
+        if (!features.length) {
+          return;
+        }
+
+        var feature = features[0];
+
+        var popup = new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(feature.geometry.coordinates)
+          .setHTML(
+            '<h3>'+feature.properties.username+'</h3><br /><p><h2><b>' + feature.properties.title + '</b></h2><br />Skills: ' + feature.properties.skills + '<br /><p>Organisation: ' + feature.properties.organisation  + '<br /></p>')
+          .setLngLat(feature.geometry.coordinates)
+          .addTo(map);
+      });
+
+      map.on('mouseenter', 'poi-engineer', function () {
+          map.getCanvas().style.cursor = 'pointer';
+      });
+
+      map.on('mouseleave', 'poi-engineer', function () {
+          map.getCanvas().style.cursor = '';
+      });
     });
 
   }
@@ -209,8 +298,20 @@ class Test extends Component {
 
     return (
       <div>
-
+        <div>Icons made by
+            <a href="https://www.flaticon.com/authors/freepik" title="Nurse">Nurse</a> from
+            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+            <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0"
+                target="_blank">CC 3.0 BY</a>
+        </div>
+        <div>Icons made by
+            <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from
+            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+            <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0"
+                target="_blank">CC 3.0 BY</a>
+        </div>
         <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+        <nav id='filter-group' className='filter-group'></nav>
       </div>
     );
   }
