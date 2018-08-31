4d157d44bac1ba660e98f91fdd2f560bba914ee0
Tim van Vliet <dev.timvanvliet@gmail.com>
2018-08-31 11:27:57 +0200

    Fix active disasters bar

--- client/src/components/disasters/active_disaster/activeDisasterBar.jsx
+++ client/src/components/disasters/active_disaster/activeDisasterBar.jsx
@@ -4,49 +4,18 @@ export default function activeDisasterBar(props) {
   console.log(props)
   const tabs = ['Notifications', 'Map', 'Chat'];
   return (
-    // <ul className='boxes'>
-    //   {tabs.map(tab => {
-    //     return (
-    //       <li
-    //         key={tab}
-    //         className={props.selectedTab ? 'selected-box' : null}
-    //         style={tab === props.selectedTab ? { color: 'red' } : null}
-    //         onClick={props.updateTab.bind(null, tab )}
-    //         >
-    //         {tab}
-    //       </li>
-    //     );
-    //   })}
-    // </ul>
-
-    <div className="boxes">
-      <div className="unselected-box">
-        <button
-          key={'Notifications'}
-          onClick={props.updateTab.bind(null, true)}
-          className={props.selectedTab ? 'selected-box' : null}
-        >
-          NOTIFICATIONS
-  </button>
-      </div>
-      <div className="unselected-box">
-        <button
-          key={'Map'}
-          onClick={props.updateTab.bind(null, false)}
-          className={props.selectedTab ? null : "selected-box"}
-        >
-          MAP
-  </button>
-      </div>
-      <div className="unselected-box">
-        <button
-          key={'Chat'}
-          onClick={props.updateTab.bind(null, false)}
-          className={props.selectedTab ? null : "selected-box"}
-        >
-          CHAT
-  </button>
-      </div>
+    <div className='boxes'>
+      {tabs.map(tab => {
+        return (
+          <button
+            key={tab}
+            className={tab === props.selectedTab ? 'selected-box' : 'unselected-box'}
+            onClick={props.updateTab.bind(null, tab )}
+            >
+            {tab}
+          </button>
+        );
+      })}
     </div>
   );
 }
